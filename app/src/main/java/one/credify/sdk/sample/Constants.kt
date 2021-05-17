83ddfc90b714fa3b6b3449704df5883f0c54cb03
Gioan <gioan.le@credify.one>
2021-05-17 18:06:13 +0700

    Updated Constants file

--- app/src/main/java/one/credify/sdk/sample/Constants.kt
+++ app/src/main/java/one/credify/sdk/sample/Constants.kt
@@ -4,31 +4,19 @@ import one.credify.sdk.core.model.Environment
 import java.util.*
 
 object Constants {
-    const val MARKET_NAME = "sendo"
-    const val API_KEY = "WaXSjOqK0JqSOH1VJ6Op1kkQTdPAhffv5bflck7SzwRjCK0MqUmjSHyvfAan3djf"
-    val ENVIRONMENT = Environment.DEVELOP
-
-//    val GET_USER_URL: String
-//        get() {
-//            return "https://sandbox-demo-api.credify.dev/${MARKET_NAME.toLowerCase(Locale.ENGLISH)}/demo-user"
-//        }
-//
-//    val PUSH_CLAIMS_URL: String
-//        get() {
-//            return "https://sandbox-demo-api.credify.dev/${MARKET_NAME.toLowerCase(Locale.ENGLISH)}/push-claims"
-//        }
-//
-//    const val GET_ACCESS_TOKEN_BY_API_KEY_URL =  "https://sandbox-api.credify.dev/v1/token"
+    const val MARKET_NAME = "" // Your name
+    const val API_KEY = "" // Your API key
+    val ENVIRONMENT = Environment.SANDBOX // Should not be changed in this example
 
     val GET_USER_URL: String
         get() {
-            return "https://dev-demo-api.credify.ninja/${MARKET_NAME.toLowerCase(Locale.ENGLISH)}/demo-user"
+            return "https://sandbox-demo-api.credify.dev/${MARKET_NAME.toLowerCase(Locale.ENGLISH)}/demo-user"
         }
 
     val PUSH_CLAIMS_URL: String
         get() {
-            return "https://dev-demo-api.credify.ninja/${MARKET_NAME.toLowerCase(Locale.ENGLISH)}/push-claims"
+            return "https://sandbox-demo-api.credify.dev/${MARKET_NAME.toLowerCase(Locale.ENGLISH)}/push-claims"
         }
 
-    const val GET_ACCESS_TOKEN_BY_API_KEY_URL =  "https://dev-api.credify.ninja/v1/token"
+    const val GET_ACCESS_TOKEN_BY_API_KEY_URL =  "https://sandbox-api.credify.dev/v1/token"
 }
