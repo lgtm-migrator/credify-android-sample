99b4d385fd6b387654255f926b0d82f1d74e70d2
Gioan <gioan.le@credify.one>
2021-12-08 15:32:53 +0700

    Fixed code preview [18322]

--- app/src/main/java/one/credify/sdk/sample/requester/MarketRequester.kt
+++ app/src/main/java/one/credify/sdk/sample/requester/MarketRequester.kt
@@ -11,47 +11,6 @@ import one.credify.sdk.sample.response.AccessTokenResponse
 import org.json.JSONObject
 
 class MarketRequester {
-    fun getAccessToken(
-        context: Context,
-        url: String,
-        apiKey: String,
-        onRequest: () -> Unit,
-        onResult: (accessToken: String?) -> Unit
-    ) {
-        onRequest()
-
-        Log.d("MarketRequester", "Url: $url")
-
-        // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(context)
-
-        // Request a string response from the provided URL.
-        val stringRequest = object : StringRequest(
-            Method.POST,
-            url,
-            { response ->
-                // Display the first 500 characters of the response string.
-                val gson = Gson()
-                val accessToken = gson.fromJson(response, AccessTokenResponse::class.java)
-
-                onResult(accessToken.data?.accessToken)
-            },
-            {
-                // Error
-                Log.d("MarketRequester", "$it")
-                onResult(null)
-            }
-        ) {
-            override fun getHeaders(): MutableMap<String, String> {
-                return HashMap<String, String>().apply {
-                    put("X-API-KEY", apiKey)
-                }
-            }
-        }
-
-        queue.add(stringRequest)
-    }
-
     fun pushClaimToken(
         context: Context,
         marketInfo: MarketInfo,
@@ -60,53 +19,33 @@ class MarketRequester {
         onRequest: () -> Unit,
         onResult: (isSuccess: Boolean) -> Unit
     ) {
-        getAccessToken(
-            context = context,
-            url = marketInfo.getAccessTokenUrl,
-            apiKey = marketInfo.apiKey,
-            onRequest = {
-                onRequest()
+        onRequest()
+
+        val url = marketInfo.getPushClaimTokenUrl
+
+        Log.d("UserRequester", "Url: $url")
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(context)
+
+        // Request a string response from the provided URL.
+        val stringRequest = object : JsonObjectRequest(
+            Method.POST,
+            url,
+            JSONObject().apply {
+                put("id", localId)
+                put("credify_id", credifyId)
             },
-            onResult = OnResult@ { accessToken ->
-                if (!accessToken.isNullOrEmpty()) {
-                    val url = marketInfo.getPushClaimTokenUrl
-
-                    Log.d("UserRequester", "Url: $url")
-
-                    // Instantiate the RequestQueue.
-                    val queue = Volley.newRequestQueue(context)
-
-                    // Request a string response from the provided URL.
-                    val stringRequest = object : JsonObjectRequest(
-                        Method.POST,
-                        url,
-                        JSONObject().apply {
-                            put("id", localId)
-                            put("credify_id", credifyId)
-                        },
-                        { _ ->
-                            onResult(true)
-                        },
-                        {
-                            // Error
-                            Log.d("MarketRequester", "pushClaimToken -> Error: $it")
-                            onResult(false)
-                        }
-                    ) {
-                        override fun getHeaders(): MutableMap<String, String> {
-                            return HashMap<String, String>().apply {
-                                put("Authorization", accessToken)
-                            }
-                        }
-                    }
-
-                    queue.add(stringRequest)
-
-                    return@OnResult
-                }
-
+            { _ ->
+                onResult(true)
+            },
+            {
+                // Error
+                Log.d("MarketRequester", "pushClaimToken -> Error: $it")
                 onResult(false)
             }
-        )
+        ) {}
+
+        queue.add(stringRequest)
     }
 }
