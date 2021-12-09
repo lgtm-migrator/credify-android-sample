6b8d49ceb3fdcf45e8dde61519811b2a7688910c
Gioan <gioan.le@credify.one>
2021-12-09 16:21:54 +0700

    Update market info [18322]

--- app/src/main/java/one/credify/sdk/sample/Constants.kt
+++ app/src/main/java/one/credify/sdk/sample/Constants.kt
@@ -4,8 +4,9 @@ import one.credify.sdk.core.model.Environment
 import java.util.*
 
 object Constants {
-    const val MARKET_NAME = "Housecare" // Your name
+    const val MARKET_NAME = "Sample Market" // Your name
     const val API_KEY = "7kx6vx9p9gZmqrtvHjRTOiSXMkAfZB3s5u3yjLehQHQCtjWrjAk9XlQHR2IOqpuR" // Your API key
+    const val PATH = "service-j"
     val ENVIRONMENT = Environment.SANDBOX // Should not be changed in this example
 
     const val INTENT_USER_PROFILE = "INTENT_USER_PROFILE"
